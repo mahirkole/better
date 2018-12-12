a3c0898b4f8193bb912cf96c736a2a8536269ee2
Mahir Köle <mahirkole@gmail.com>
2018-12-12 23:40:08 +0300

    Data types declared

---
+++ src/main/java/com/mahirkole/better/Event.java
@@ -0,0 +1,15 @@
+package com.mahirkole.better;
+
+import java.sql.Time;
+import java.util.Date;
+
+public class Event {
+
+    private Long iddiaId;
+    private EventType type;
+    private Date date;
+    private Integer matchCode;
+    private Short mbs;
+    private Team home;
+    private Team away;
+}
